f6827ef73ea928e800d54f338dab272bf1391403
MunisaAbdullayeva <abdullayevam385@gmail.com>
2024-06-24 11:26:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// let ism = "Munisa"
+// let familya = "Abdullayeva"
+
+// console.log ( ism + " " + familya )
+
+// let ism1 = "Bekzod";
+
+// let son = 12344;
+
+// let showcase = {
+//     background: "red",
+// };
+
+// let son2 = 12345678900987654321n;
+
+// let yoqqiymat = null;
+
+// let qiymat = 3 > 2; //boolean
+
+// console.log(ism1 + " " + typeof ism1 )
+// console.log(son + " " + typeof son )
+// console.log(showcase + " " + typeof showcase )
+// console.log(son2 + " " + typeof son2 )
+// console.log(yoqqiymat + " " + typeof yoqqiymat )
+// console.log(qiymat + " " + typeof qiymat )
+
+
+let ism2 = prompt("ism yozing: ");
+let familya2 = prompt("familyangizni yozing: ");
+let yosh = prompt("yoshingizni yozing: ");
+let address = prompt("address yozing: ");
+let fan = prompt("yoqtrgan faningizni yozing: ");
+
+
+console.log("ismi: " + ism2)
+console.log("familya: " + familya2)
+console.log("yosh: " + yosh)
+console.log("address: " + address)
+console.log("fan: " + fan)
+
+
